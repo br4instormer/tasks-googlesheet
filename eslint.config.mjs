a30876a3aa94030271fb44c7694ab80fc78b6ed9
br4instormer <br4instormer@gmail.com>
2025-08-16 14:10:36 +0300

    Add eslint & jsdoc plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import jsdoc from "eslint-plugin-jsdoc";
+import googleAppsScript from "eslint-plugin-googleappsscript";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  jsdoc.configs["flat/recommended"],
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "script",
+      globals: googleAppsScript.environments.googleappsscript.globals,
+    },
+  },
+]);
